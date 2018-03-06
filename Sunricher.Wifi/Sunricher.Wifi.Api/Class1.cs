531f865c5d0b043380115f854673ee85a930bc40
arxen42 <arxen42@tutanota.com>
2018-03-06 12:32:56 +0300

    Solution created

---
+++ Sunricher.Wifi/Sunricher.Wifi.Api/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Sunricher.Wifi.Api
+{
+    public class Class1
+    {
+    }
+}
---
+++ Sunricher.Wifi/Sunricher.Wifi.CommandLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sunricher.Wifi.CommandLine
+{
+    internal static class Program
+    {
+        private static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
