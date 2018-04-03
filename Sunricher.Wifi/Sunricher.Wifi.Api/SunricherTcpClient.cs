f508c911f17c1169728edfe1253fa3ae6dd6f22e
arxen42 <arxen42@tutanota.com>
2018-04-03 18:55:47 +0300

    Reworked SunricherTcpClient

--- Sunricher.Wifi/Sunricher.Wifi.Api/SunricherTcpClient.cs
+++ Sunricher.Wifi/Sunricher.Wifi.Api/SunricherTcpClient.cs
@@ -14,29 +14,54 @@ namespace Sunricher.Wifi.Api
 	/// </remarks>
 	public class SunricherTcpClient : IDisposable
 	{
-		private readonly TcpClient _tcpClient;
+		private readonly String _host;
+		private readonly Int32 _port;
+		private TcpClient _tcpClient;
 
+		/// <summary>
+		///     Constructs new SunricherTcpClient using given TcpClient. TcpClient must be already connected.
+		/// </summary>
+		/// <remarks>
+		///     Use this constructor if you need to configure TcpClient manually (SendTimeout, SendBufferSize, connect).
+		/// </remarks>
+		public SunricherTcpClient(TcpClient tcpClient)
+		{
+			if (tcpClient == null)
+				throw new ArgumentNullException("Provided TCP client is null.");
+			if (!tcpClient.Connected)
+				throw new ArgumentException("Provided TCP client is not connected.");
+
+			_tcpClient = tcpClient;
+		}
+
+		/// <summary>
+		///     Constructs new SunricherTcpClient with given host and port. Attempts to connect immediately.
+		/// </summary>
 		public SunricherTcpClient(String host, Int32 port)
 		{
-			_tcpClient = new TcpClient(host, port);
-			DelayAfterMessage = TimeSpan.FromMilliseconds(100);
+			if (String.IsNullOrEmpty(host))
+				throw new ArgumentNullException("Host is null or empty.");
+
+			_host = host;
+			_port = port;
 		}
 
 		/// <summary>
 		///     Delay after message is sent. Default is 100 milliseconds.
 		/// </summary>
-		public TimeSpan DelayAfterMessage { get; set; }
-
-		/// <summary>
-		///     Returns whether the underlying TCP client is connected.
-		/// </summary>
-		public Boolean Connected => _tcpClient.Connected;
+		public TimeSpan DelayAfterMessage { get; set; } = TimeSpan.FromMilliseconds(100);
 
 		/// <summary>
 		///     Sends given message to host using TcpClient.
 		/// </summary>
 		public void SendMessage(Byte[] message)
 		{
+			if (_tcpClient == null)
+				_tcpClient = new TcpClient(_host, _port);
+
+			if (!_tcpClient.Connected)
+				_tcpClient.Connect(_host, _port);
+
 			_tcpClient.GetStream().Write(message, 0, message.Length);
 			Thread.Sleep(DelayAfterMessage);
 		}
@@ -46,6 +71,12 @@ namespace Sunricher.Wifi.Api
 		/// </summary>
 		public async Task SendMessageAsync(Byte[] message, CancellationToken cancellationToken)
 		{
+			if (_tcpClient == null)
+			{
+				_tcpClient = new TcpClient();
+				await _tcpClient.ConnectAsync(_host, _port);
+			}
+
 			await _tcpClient.GetStream().WriteAsync(message, 0, message.Length, cancellationToken);
 			await Task.Delay(DelayAfterMessage, cancellationToken);
 		}
@@ -55,8 +86,7 @@ namespace Sunricher.Wifi.Api
 		/// </summary>
 		public async Task SendMessageAsync(Byte[] message)
 		{
-			await _tcpClient.GetStream().WriteAsync(message, 0, message.Length);
-			await Task.Delay(DelayAfterMessage);
+			await SendMessageAsync(message, CancellationToken.None);
 		}
 
 		public void Dispose()
--- Sunricher.Wifi/Sunricher.Wifi.CommandLine/Program.cs
+++ Sunricher.Wifi/Sunricher.Wifi.CommandLine/Program.cs
@@ -3,6 +3,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using Sunricher.Wifi.Api;
 
+// ReSharper disable AccessToDisposedClosure
+
 namespace Sunricher.Wifi.CommandLine
 {
 	internal static class Program
@@ -12,24 +14,32 @@ namespace Sunricher.Wifi.CommandLine
 			var messagesProvider = new MessagesProvider(new MessagesGenerator());
 			var random = new Random();
 
+			//Just an example of how to use API, pretty bad example
 			using (var client = new SunricherTcpClient("192.168.12.133", ApiConstants.DefaultTcpPort))
 			{
 				var cts = new CancellationTokenSource();
 				var ct = cts.Token;
 				var task = Task.Run(() =>
 				{
-					client.SendMessage(messagesProvider.PowerOn());
-
-					while (true)
+					try
 					{
-						if (ct.IsCancellationRequested)
-							break;
+						client.SendMessageAsync(messagesProvider.PowerOn(), ct).Wait(ct);
+						while (true)
+						{
+							if (ct.IsCancellationRequested)
+								break;
 
-						client.SendMessage(messagesProvider.SetR((Byte) random.Next(0, 100)));
-						client.SendMessage(messagesProvider.SetG((Byte) random.Next(0, 100)));
-						client.SendMessage(messagesProvider.SetB((Byte) random.Next(0, 100)));
-						client.SendMessage(messagesProvider.SetBrightness((Byte) random.Next(0, 255)));
-						Thread.Sleep(1000);
+							client.SendMessageAsync(messagesProvider.SetR((Byte) random.Next(0, 100)), ct).Wait(ct);
+							client.SendMessageAsync(messagesProvider.SetG((Byte) random.Next(0, 100)), ct).Wait(ct);
+							client.SendMessageAsync(messagesProvider.SetB((Byte) random.Next(0, 100)), ct).Wait(ct);
+							client.SendMessageAsync(messagesProvider.SetBrightness((Byte) random.Next(0, 255)), ct).Wait(ct);
+							Thread.Sleep(1000);
+						}
+					}
+					catch (Exception)
+					{
+						cts.Cancel();
+						throw;
 					}
 				}, ct);
 
